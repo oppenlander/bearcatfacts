f9d90d9b7c0e0f0877c63f502b1d45b15b5c5f51
Andrew Oppenlander <andrew@oppenlander.co>
2014-04-06 11:52:08 -0400

    Added faxes

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ var express = require('express');
 var app = express();
 
 var facts = [
-  "Bearcats are also called 'Binturongs'",
+  "Bearcats are also called Binturongs",
   "Bearcats have a muck gland that emits an odor reminisant of popcorn.",
   "Binturongs are omnivorous, feeding on small mammals, birds, fish, earthworms, insects and fruits.",
-  "The bearcat's genus name is Arctictis.'"
+  "The bearcat's genus name is Arctictis.",
+  "A bearcat is called a tenturun in Riau, Indonesia.",
+  "Bearcats have a prehensile tail.",
+  "Bearcats us their tail to communicate through scent glands.",
+  "Captive bearcats are particularly fond of plantains, but would also eat fowls' heads and eggs.",
+  "Bearcats are omnivorous, feeding on small mammals, birds, fish, earthworms, insects and fruits.",
+  "In captivity, the bearcat has been noted for its intelligence as well as its curious disposition.",
+  "Bearcats are native to South and Southeast Asia."
 ];
 
 app.get('/', function(req, res) {
