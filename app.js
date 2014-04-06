258e864bd772f29a3c189b5a44e8beeafa1f65fa
Ross Hadden <rosshadden@gmail.com>
2014-04-06 12:48:46 -0400

    And there were many many cats.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var express = require('express');
+var express = require('express'),
+    request = require('request');
 
 var app = express();
 
@@ -13,12 +14,39 @@ var facts = [
   "Captive bearcats are particularly fond of plantains, but would also eat fowls' heads and eggs.",
   "Bearcats are omnivorous, feeding on small mammals, birds, fish, earthworms, insects and fruits.",
   "In captivity, the bearcat has been noted for its intelligence as well as its curious disposition.",
-  "Bearcats are native to South and Southeast Asia."
+  "Bearcats are native to South and Southeast Asia.",
+  "{catfacts}"
 ];
+
+var external = {
+  catfacts: function(req, res) {
+    request({
+      url: 'http://catfacts-api.appspot.com/api/facts',
+      json: true
+    }, function (error, response, data) {
+      if (!error && response.statusCode == 200) {
+        var fact = data.facts[0];
+        fact = fact.replace(/\b([Cc])(ats?)\b/g, function(str, c, at) {
+          var b = (c === "C") ? "B" : "b";
+          return b + "earc" + at;
+        });
+
+        res.send(fact);
+      }
+    });
+  }
+};
 
 app.get('/', function(req, res) {
   var len = facts.length;
-  res.send(facts[Math.floor(Math.random()*len)]);
+  var fact = facts[Math.floor(Math.random() * len)];
+  var match = fact.match(/\{(\w+)\}/);
+
+  if (match !== null) {
+    return external[match[1]](req, res);
+  }
+
+  res.send(fact);
 });
 
 var server = app.listen(5000, function() {
